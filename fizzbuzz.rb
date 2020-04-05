d61191cf4993c129a396e53e288d9551009883f6
Louis Liu <louis.liu2013@gmail.com>
2020-04-05 23:09:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,14 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  end
+end
+
+def fizzbuzz(int)
+  if int % 5 == 0
+    "Buzz"
+end
+
+def fizzbuzz(int)
+  if int % 3 & 5 == 0
+    "FizzBuzz"
 end
